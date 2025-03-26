4257d7684631ac54d1e8344226fff7a89848d12a
Karen Sarai Morales Montiel <karen.sarai.morales@gmail.com>
2025-03-25 18:42:58 -0600

    Fixed link to backend

--- src/services/kittys/kittyService.jsx
+++ src/services/kittys/kittyService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: `:${import.meta.env.BASE_URL}:${import.meta.env.BACKEND_PORT}/api`, // Vite proxy target
+    baseURL: `${import.meta.env.BASE_URL}:${import.meta.env.BACKEND_PORT}/api`, // Vite proxy target
     headers: {
       'x-api-key': import.meta.env.VITE_API_KEY,
       'Accept': 'application/json', // Explicitly ask for JSON
@@ -18,7 +18,7 @@ const getRandomKitty = async () => {
             'Content-Type': 'application/json'
         }
     }); 
-    console.log(response)
+    console.log(`${import.meta.env.BASE_URL}:${import.meta.env.BACKEND_PORT}/api`)
     return response.data;
   } catch (error) {
     console.error("❌ Error fetching random kitty:", error.response?.data || error.message);
