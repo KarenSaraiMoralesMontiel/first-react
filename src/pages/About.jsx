465d34f45e1cf781726d1bd43fbcf002a294103d
Karen Sarai Morales Montiel <karen.sarai.morales@gmail.com>
2025-03-31 10:43:15 -0600

    Added infomration about Kitty

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,14 @@ function About() {
     return (
         <div className="container">
     <div className="card">
-        <p>I adopted Kitty (that's her name, yes, don't ask) while I was doing a course on React and Nodejs and found myself wanting to have a place so I could see photos of my cat everywhere.
+        <p>I adopted Kitty (that's her name, yes, don't ask) while I was doing a course on React and Nodejs and found myself wanting to have a place so I could see photos of my cat everywhere. 
+            I thought why not. In case you want to know random tidbits of my cat:
+            <ul>
+                <li>Likes Garfield</li>
+                <li>She is terrible hunter but I love her anyway</li>
+                <li>She likes destroying sofas</li>
+                <li>She is orange so she is crazy</li>
+            </ul>
         </p>
         
     </div>
